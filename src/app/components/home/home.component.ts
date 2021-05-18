2570342b85913766cbeecbf2102f430aab345587
Eric Newman <googlnewman@gmail.com>
2021-05-18 17:24:21 -0500

    changed color and fixed the page length issue aka screen size issue

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -16,82 +16,106 @@ export class HomeComponent implements OnInit {
   
   /* or the classic JavaScript object */
   particlesOptions = {
-      background: {
-          color: {
-              value: "#0d47a1"
-          }
+    "background": {
+      "color": {
+        "value": "#0d47a1"
       },
-      fpsLimit: 60,
-      interactivity: {
-          detectsOn: "canvas",
-          events: {
-              onClick: {
-                  enable: true,
-                  mode: "push"
-              },
-              onHover: {
-                  enable: true,
-                  mode: "repulse"
-              },
-              resize: true
-          },
-          modes: {
-              bubble: {
-                  distance: 400,
-                  duration: 2,
-                  opacity: 0.8,
-                  size: 40
-              },
-              push: {
-                  quantity: 4
-              },
-              repulse: {
-                  distance: 200,
-                  duration: 0.4
-              }
+      "position": "50% 50%",
+      "repeat": "no-repeat",
+      "size": "cover"
+    },
+    "fullScreen": {
+      "enable": true,
+      "zIndex": 1
+    },
+    "interactivity": {
+      "events": {
+        "onClick": {
+          "enable": true,
+          "mode": "push"
+        },
+        "onHover": {
+          "enable": true,
+          "mode": "grab",
+          "parallax": {
+            "enable": true,
+            "force": 60
           }
+        }
       },
-      particles: {
-          color: {
-              value: "#ffffff"
-          },
-          links: {
-              color: "#ffffff",
-              distance: 150,
-              enable: true,
-              opacity: 0.5,
-              width: 1
-          },
-          collisions: {
-              enable: true
-          },
-          move: {
-              direction: "none",
-              enable: true,
-              outMode: "bounce",
-              random: false,
-              speed: 6,
-              straight: false
-          },
-          number: {
-              density: {
-                  enable: true,
-                  value_area: 800
-              },
-              value: 80
-          },
-          opacity: {
-              value: 0.5
-          },
-          shape: {
-              type: "circle"
-          },
-          size: {
-              random: true,
-              value: 5
+      "modes": {
+        "bubble": {
+          "distance": 400,
+          "duration": 2,
+          "opacity": 0.8,
+          "size": 40
+        },
+        "grab": {
+          "distance": 400
+        }
+      }
+    },
+    "particles": {
+      "color": {
+        "value": "#ffffff"
+      },
+      "links": {
+        "color": {
+          "value": "#ffffff"
+        },
+        "distance": 150,
+        "enable": true,
+        "opacity": 0.4
+      },
+      "move": {
+        "attract": {
+          "rotate": {
+            "x": 600,
+            "y": 1200
           }
+        },
+        "enable": true,
+        "outModes": {
+          "bottom": "out",
+          "left": "out",
+          "right": "out",
+          "top": "out"
+        }
       },
-      detectRetina: true
+      "number": {
+        "density": {
+          "enable": true
+        }
+      },
+      "opacity": {
+        "random": {
+          "enable": true
+        },
+        "value": {
+          "min": 0.1,
+          "max": 0.5
+        },
+        "animation": {
+          "enable": true,
+          "speed": 3,
+          "minimumValue": 0.1
+        }
+      },
+      "size": {
+        "random": {
+          "enable": true
+        },
+        "value": {
+          "min": 0.1,
+          "max": 10
+        },
+        "animation": {
+          "enable": true,
+          "speed": 20,
+          "minimumValue": 0.1
+        }
+      }
+    }
   };
 
   particlesLoaded(container: Container): void {
